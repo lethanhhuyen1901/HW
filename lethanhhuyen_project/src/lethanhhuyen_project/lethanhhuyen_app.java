9d5484315b87f99e2f2de337c0716dcd3b0d17b1
lethanhhuyen <2121051314@student.humg.edu.vn>
2023-02-17 23:43:14 +0700

    V1

---
+++ lethanhhuyen_project/src/lethanhhuyen_project/lethanhhuyen_app.java
@@ -0,0 +1,9 @@
+package lethanhhuyen_project;
+
+public class lethanhhuyen_app {
+	public static void main(String[]args)
+	{
+		System.out.println("Hello World");
+		System.out.println("Le Thanh Huyen hoc Java");	
+	}
+}
